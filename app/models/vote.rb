61b17390ec76101739d292f0c4366a2660454623
Florent Allain <allain.florent@gmail.com>
2016-01-02 14:43:10 +0100

    validate_ssn_key

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,18 +1,20 @@
 class Vote < ActiveRecord::Base
-  validates :ssn, presence: true, uniqueness: true, length: {is: 15 } # length 13 if key is not include
+  validates :ssn, presence: true, uniqueness: true, numericality: true, length: {is: 15 } # length 13 if key is not included
   validates :choice, presence: true
+  validate :has_valid_key
 
+  #before_create :has_valid_key
 
-  def has_valid_key?
-    #ensure the ssn key is valid through formula : 97 - (ssn % 97)
-    self.ssn.last(2).to_i == 97 - (self.ssn.to_i % 97)
+  def has_valid_key
+    unless self.ssn.last(2).to_i == 97 - (self.ssn[0...-2].to_i % 97)
+      errors.add(:ssn, "n'a pas la clé valide")
+    end
   end
 
   def is_male?
     self.ssn.slice(0).to_i == 1
   end
 end
-
 
 # my ssn = 1830393045014 ; ssnkey = 10
 # to know ssnkey => 97 - (ssn % 97)
